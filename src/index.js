7a90c1b131c5812ae6c63928fabbf3ae21e6d75d
Christopher Wong <cw30355@gmail.com>
2023-02-05 15:36:01 -0500

    Finally completed this lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const newTaskDescription = document.querySelector("#new-task-description")
+const taskList = document.querySelector("#tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    createTask(newTaskDescription)
+  })
 });
+
+function createTask(todo){
+  let newTask = document.createElement("li")
+  newTask.innerText = `${todo.value} `
+  createButton(newTask)
+  taskList.appendChild(newTask)
+}
+
+function createButton(task){
+  let taskButton = document.createElement("button")
+  taskButton.innerText = "x"
+  task.appendChild(taskButton)
+  taskButton.addEventListener("click", (e) => deleteTask(e))
+}
+
+function deleteTask(e){
+  const selectedTask = e.target.parentElement
+  taskList.removeChild(selectedTask)
+}
